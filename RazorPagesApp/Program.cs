e38de34df68798a222b5a8262b1b0a63cb6b487d
Ilya Omelchenko <109640444+mrchysen@users.noreply.github.com>
2024-06-10 07:23:14 +0700

    fix: delete useless comment

--- RazorPagesApp/Program.cs
+++ RazorPagesApp/Program.cs
@@ -32,6 +32,4 @@ app.MapRazorPages(); // Routing for razor pages
 app.UseMiddleware<AuntificationMiddelware>();
 app.UseMiddleware<AdministrationControlMiddelware>();
 
-Console.WriteLine("Running application...");
-
 app.Run();  
